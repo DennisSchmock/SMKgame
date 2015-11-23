fd24ee0e9303de54ba1295471fdedb1a55033512
dennisschmock <dennisschmock@10.50.130.183>
2015-11-23 09:49:50 +0100

    Denne udgave er god

--- src/SMKGUI/GUI_For_Queing.java
+++ src/SMKGUI/GUI_For_Queing.java
@@ -1,10 +1,52 @@
-package SMKGUI;
+package Control;
+
+import SMKGUI.Smartphone;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import javax.swing.Timer;
 
 public class GUI_For_Queing {
+
+    private static Timer timer;
+    private static int countDown;
+    private static boolean gameBool = true;
 
     public static void main(String[] args) {
         // Smartphone GUI
         Smartphone smartphone = new Smartphone();
         smartphone.setVisible(true);
+        
+       
+        theTimer(smartphone.jProgressBar1, 300);
+            
+        
+
+    }
+
+    public static boolean theTimer(final JProgressBar j, final int max) {
+       
+        new JFrame().setVisible(true);
+        ActionListener actListner = new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                countDown++;
+
+                j.setValue(countDown);
+                if (countDown > max) {
+
+                    timer.stop();
+                    gameBool = false;
+                    
+                }
+            }
+
+        };
+
+        timer = new Timer(100, actListner);
+        timer.start();
+        return true;
     }
 }
