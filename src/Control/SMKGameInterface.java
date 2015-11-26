38d8d9f929f6f6488828d70dd6dd599773fe7e85
PWC <PWC@10.50.130.192>
2015-11-26 09:51:40 +0100

    Merge origin/master
    Conflicts:
    	src/Control/SMKmain.java

--- src/Control/SMKGameInterface.java
+++ src/Control/SMKGameInterface.java
@@ -1,18 +1,9 @@
 package Control;
 
 import Engine.User;
-import java.util.ArrayList;
 
 public interface SMKGameInterface {
 
-    public void timer();
-    public String randomQuestion(); //done
-    public String load(); //done
-    public boolean checkGuess(int correct); //done
-    public ArrayList<User> returnRank();
-    public String[] answerOptions();
-    public void setPoints(int points);
-    public int getPoints();
-    public void pushAnswerButton(int i, User user);
-
+    public boolean checkGuess(int correct); // Checkguess with int from pushAnswerButton
+    public void pushAnswerButton(int i, User user); // Answer button smartphone
 }
--- src/Control/SMKmain.java
+++ src/Control/SMKmain.java
@@ -2,24 +2,11 @@ package Control;
 
 import Engine.Engine;
 import Engine.InputOutput;
-import Engine.Question;
 import Engine.User;
 import SMKGUI.Smartphone;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import javax.swing.Timer;
 
 public class SMKmain {
 
-//    private static Timer timer;
-//    private static int countIntro = 200;
-//    private static int countUser = 200;
-//    private static int countAnswer = 300;
-//    private static int countScore = 200;
-//    private static int countRanking = 200;
-//    private static int numberQ = 5;
-//    private static int countUp;
-//    private static Question currentQuestion;
     private static User user;
     private static Engine engine;
     private static InputOutput io;
@@ -31,13 +18,10 @@ public class SMKmain {
         InputOutput.shuffleQuestions();
         InputOutput.shuffleUsers();
 
-        user = InputOutput.loadUsers().get(0);
-        smartphone = new Smartphone(user);
-        engine = new Engine(smartphone,InputOutput.loadUsers());
-        smartphone.setVisible(true);
-        Engine.backgroundMusic("src/SMKGUI_pics/lyk.wav");
-        
-
+        user = InputOutput.loadUsers().get(0); // Load users
+        smartphone = new Smartphone(user); // Initialize smartphone class
+        engine = new Engine(smartphone, InputOutput.loadUsers()); // Calling engine constructor
+        smartphone.setVisible(true); // Make smartphone visible
+        Engine.backgroundMusic("src/SMKGUI_pics/lyk.wav"); // Load background music
     }
-
 }
